41362ba42c3d8d53c4f3697a7e58c30438a15124
AnnaNThomas <annathomas2003ned@gmail.com>
2024-07-01 00:49:16 +0530

    Delete

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-
+import './Delete.css'
 
 const Delete = () => {
   return (
     <div>
-      
-        <div className="container">
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                <div className="row g-3">
-                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <label for="" class="form-label">Vehicle Number</label>
-                    <input type="text" className="form-contol" />  
-                    </div>
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button class="btn btn-danger">Delete</button>   
-                    </div>
-                </div>
-
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <div className="row g-3">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Vehicle Number</label>
+            <input type="text" className="form-control" />
             </div>
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <buttton className="btn btn-danger">Delete</buttton>
+            </div>
+          </div>
+          </div>
         </div>
+      </div>
     </div>
+
+    
   )
 }
 
